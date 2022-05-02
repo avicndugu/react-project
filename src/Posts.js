0e653a6eb608f9a1f6e4b8e647a5d04b94ba2d85
avicnotes <78015849+avicnotes@users.noreply.github.com>
2022-05-02 20:31:25 +0300

    Adding filter to post component

--- src/Posts.js
+++ src/Posts.js
@@ -8,23 +8,42 @@ function Posts() {
     const fetchData = () => {
       fetch('http://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
-        .then((data) => { setData(data) })
+        .then((data) => { setData(data)})
+        // Filtering the data immediately
+        // .then((data) => { setData(data.filter( content => content.userId == 10 ))})
       };
       fetchData();
   },[]);
 
+  let filteredData =data.filter( content => content.userId == 10 )
+  console.log(filteredData);
+
   return (
     <div className="App">
       <div>
+        {/*
+          <div>
+            {(data.map(item => (
+              <>
+                <p key={item.userId}>{item.userId}</p>
+                <li key={item.userId}>{item.title}</li>
+                <li key={item.userId}>{item.body}</li>
+              </>
+            )))}
+          </div>
+      */}
+      {/* Filtering the data in JSX */}
         <div>
-          {data.map(item => (
-            <>
-              <p key={item.userId}>{item.userId}</p>
-              <li key={item.userId}>{item.title}</li>
-              <li key={item.userId}>{item.body}</li>
-            </>
-          ))}
+          {data.filter( person => person.userId === 1).map(
+            item => (
+              <>
+                <h1>{item.title}</h1>
+                <p>{item.body}</p>
+              </>
+            )
+          )}
         </div>
+
       </div>
     </div>
   );
